1a3636e7ff68e1a743fa44250e57c047b5bb1fa1
rebjo84 <rebjo84@ville-noumea.nc>
2013-12-16 04:38:20 +0000

    #5041 RECUP saisie d'une demande

--- src/main/java/nc/noumea/mairie/abs/service/impl/AbstractAbsenceDataConsistencyRules.java
+++ src/main/java/nc/noumea/mairie/abs/service/impl/AbstractAbsenceDataConsistencyRules.java
@@ -16,7 +16,9 @@ import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public abstract class AbstractAbsenceDataConsistencyRules implements IAbsenceDataConsistencyRules {
 	
 	protected Logger logger = LoggerFactory.getLogger(AbsRecuperationDataConsistencyRulesImpl.class);
