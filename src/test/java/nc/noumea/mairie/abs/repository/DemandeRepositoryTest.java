74d38f1a4ac72e63a70c5c6ec552ebd009d0717f
nicno85 <nicno85@ville-noumea.nc>
2013-12-18 03:55:39 +0000

    #5040 : RECUP - visualisation de mes demandes --> ajout de tests unitaires sur le repository

--- src/test/java/nc/noumea/mairie/abs/repository/DemandeRepositoryTest.java
+++ src/test/java/nc/noumea/mairie/abs/repository/DemandeRepositoryTest.java
@@ -2,12 +2,18 @@ package nc.noumea.mairie.abs.repository;
 
 import static org.junit.Assert.assertEquals;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import nc.noumea.mairie.abs.domain.Demande;
+import nc.noumea.mairie.abs.domain.DemandeRecup;
 import nc.noumea.mairie.abs.domain.RefEtat;
+import nc.noumea.mairie.abs.domain.RefTypeAbsence;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -93,6 +99,189 @@ public class DemandeRepositoryTest {
 
 		// Then
 		assertEquals(6, result.size());
+
+		absEntityManager.flush();
+		absEntityManager.clear();
+	}
+
+	@Test
+	@Transactional("absTransactionManager")
+	public void listeDemandesAgent_NoFilter_Return1Demande() {
+		// Given
+		DemandeRecup d = new DemandeRecup();
+		d.setIdAgent(9005138);
+		d.setDateDebut(new Date());
+		d.setDateFin(null);
+		d.setDuree(30);
+		absEntityManager.persist(d);
+
+		// When
+		List<Demande> result = repository.listeDemandesAgent(9005138, null, null, null);
+
+		// Then
+		assertEquals(1, result.size());
+		assertEquals("30", ((DemandeRecup) result.get(0)).getDuree().toString());
+
+		absEntityManager.flush();
+		absEntityManager.clear();
+	}
+
+	@Test
+	@Transactional("absTransactionManager")
+	public void listeDemandesAgent_NoFilter_Return0Demande() {
+		// Given
+		DemandeRecup d = new DemandeRecup();
+		d.setIdAgent(9005131);
+		d.setDateDebut(new Date());
+		d.setDateFin(null);
+		d.setDuree(30);
+		absEntityManager.persist(d);
+
+		// When
+		List<Demande> result = repository.listeDemandesAgent(9005138, null, null, null);
+
+		// Then
+		assertEquals(0, result.size());
+
+		absEntityManager.flush();
+		absEntityManager.clear();
+	}
+
+	@Test
+	@Transactional("absTransactionManager")
+	public void listeDemandesAgent_DateFilter_Return0Demande() throws ParseException {
+		// Given
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+		DemandeRecup d = new DemandeRecup();
+		d.setIdAgent(9005138);
+		d.setDateDebut(sdf.parse("15/05/2013"));
+		d.setDateFin(null);
+		d.setDuree(30);
+		absEntityManager.persist(d);
+
+		DemandeRecup d2 = new DemandeRecup();
+		d2.setIdAgent(9005138);
+		d2.setDateDebut(sdf.parse("15/06/2013"));
+		d2.setDateFin(null);
+		d2.setDuree(40);
+		absEntityManager.persist(d2);
+
+		// When
+		List<Demande> result = repository.listeDemandesAgent(9005138, sdf.parse("01/06/2013"), null, null);
+
+		// Then
+		assertEquals(0, result.size());
+
+		absEntityManager.flush();
+		absEntityManager.clear();
+	}
+
+	@Test
+	@Transactional("absTransactionManager")
+	public void listeDemandesAgent_DateFilter_Return1Demande() throws ParseException {
+		// Given
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+		DemandeRecup d = new DemandeRecup();
+		d.setIdAgent(9005138);
+		d.setDateDebut(sdf.parse("15/05/2013"));
+		d.setDateFin(null);
+		d.setDuree(30);
+		absEntityManager.persist(d);
+
+		DemandeRecup d2 = new DemandeRecup();
+		d2.setIdAgent(9005138);
+		d2.setDateDebut(sdf.parse("15/06/2013"));
+		d2.setDateFin(null);
+		d2.setDuree(40);
+		absEntityManager.persist(d2);
+
+		// When
+		List<Demande> result = repository.listeDemandesAgent(9005138, sdf.parse("01/06/2013"), sdf.parse("16/06/2013"),
+				null);
+
+		// Then
+		assertEquals(1, result.size());
+		assertEquals("40", ((DemandeRecup) result.get(0)).getDuree().toString());
+
+		absEntityManager.flush();
+		absEntityManager.clear();
+	}
+
+	@Test
+	@Transactional("absTransactionManager")
+	public void listeDemandesAgent_TypeFilter_Return0Demande() throws ParseException {
+		// Given
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+		RefTypeAbsence typeMaladie = new RefTypeAbsence();
+		typeMaladie.setIdRefTypeAbsence(6);
+		typeMaladie.setLabel("Maladies");
+		absEntityManager.persist(typeMaladie);
+
+		DemandeRecup d = new DemandeRecup();
+		d.setIdAgent(9005138);
+		d.setDateDebut(sdf.parse("15/05/2013"));
+		d.setDateFin(null);
+		d.setDuree(30);
+		d.setType(typeMaladie);
+		absEntityManager.persist(d);
+
+		DemandeRecup d2 = new DemandeRecup();
+		d2.setIdAgent(9005138);
+		d2.setDateDebut(sdf.parse("15/06/2013"));
+		d2.setDateFin(null);
+		d2.setDuree(40);
+		d2.setType(typeMaladie);
+		absEntityManager.persist(d2);
+
+		// When
+		List<Demande> result = repository.listeDemandesAgent(9005138, null, null, 3);
+
+		// Then
+		assertEquals(0, result.size());
+
+		absEntityManager.flush();
+		absEntityManager.clear();
+	}
+
+	@Test
+	@Transactional("absTransactionManager")
+	public void listeDemandesAgent_TypeFilter_Return1Demande() throws ParseException {
+		// Given
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		RefTypeAbsence typeMaladie = new RefTypeAbsence();
+		typeMaladie.setIdRefTypeAbsence(6);
+		typeMaladie.setLabel("Maladies");
+		absEntityManager.persist(typeMaladie);
+		RefTypeAbsence typeRecup = new RefTypeAbsence();
+		typeRecup.setIdRefTypeAbsence(3);
+		typeRecup.setLabel("Récupération");
+		absEntityManager.persist(typeRecup);
+
+		DemandeRecup d = new DemandeRecup();
+		d.setIdAgent(9005138);
+		d.setDateDebut(sdf.parse("15/05/2013"));
+		d.setDateFin(null);
+		d.setDuree(30);
+		d.setType(typeMaladie);
+		absEntityManager.persist(d);
+
+		DemandeRecup dR2 = new DemandeRecup();
+		dR2.setIdAgent(9005138);
+		dR2.setDateDebut(sdf.parse("15/06/2013"));
+		dR2.setDateFin(null);
+		dR2.setDuree(40);
+		dR2.setType(typeRecup);
+		absEntityManager.persist(dR2);
+
+		// When
+		List<Demande> result = repository.listeDemandesAgent(9005138, null, null, 3);
+
+		// Then
+		assertEquals(1, result.size());
+		assertEquals("40", ((DemandeRecup) result.get(0)).getDuree().toString());
 
 		absEntityManager.flush();
 		absEntityManager.clear();
