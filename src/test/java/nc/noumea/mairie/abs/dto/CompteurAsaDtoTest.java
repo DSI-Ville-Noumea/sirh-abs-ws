ef66de3b4e567d50d2c229fff0a717c25e15e5f9
nicno85 <nicno85@ville-noumea.nc>
2014-04-18 04:17:16 +0000

    #7305 : A54 - WS Alimentation manuelle du compteur (SIRH)

--- src/test/java/nc/noumea/mairie/abs/dto/CompteurAsaDtoTest.java
+++ src/test/java/nc/noumea/mairie/abs/dto/CompteurAsaDtoTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import nc.noumea.mairie.abs.domain.AgentAsaA48Count;
+import nc.noumea.mairie.abs.domain.AgentAsaA54Count;
 
 import org.joda.time.DateTime;
 import org.junit.Test;
@@ -17,6 +18,26 @@ public class CompteurAsaDtoTest {
 
 		// Given
 		AgentAsaA48Count ref = new AgentAsaA48Count();
+		ref.setIdAgent(9005138);
+		ref.setDateDebut(new DateTime(2013, 1, 1, 0, 0, 0).toDate());
+		ref.setDateFin(new DateTime(2013, 12, 31, 0, 0, 0).toDate());
+		ref.setTotalJours(10.0);
+
+		// When
+		CompteurAsaDto result = new CompteurAsaDto(ref);
+
+		// Then
+		assertEquals(ref.getDateDebut(), result.getDateDebut());
+		assertEquals(ref.getDateFin(), result.getDateFin());
+		assertEquals(ref.getIdAgent(), result.getIdAgent());
+		assertEquals(ref.getTotalJours(), result.getNb());
+	}
+
+	@Test
+	public void ctor_withAgentAsaA54Count() {
+
+		// Given
+		AgentAsaA54Count ref = new AgentAsaA54Count();
 		ref.setIdAgent(9005138);
 		ref.setDateDebut(new DateTime(2013, 1, 1, 0, 0, 0).toDate());
 		ref.setDateFin(new DateTime(2013, 12, 31, 0, 0, 0).toDate());
