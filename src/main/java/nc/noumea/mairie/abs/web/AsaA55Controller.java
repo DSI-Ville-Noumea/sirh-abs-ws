a2aec01b30dda3b45be7c29db8ed1665fc4bb1aa
nicno85 <nicno85@ville-noumea.nc>
2014-04-23 00:20:16 +0000

    #7136 Refactor des controllers et de la serialization JSON

--- src/main/java/nc/noumea/mairie/abs/web/AsaA55Controller.java
+++ src/main/java/nc/noumea/mairie/abs/web/AsaA55Controller.java
@@ -1,21 +1,19 @@
 package nc.noumea.mairie.abs.web;
 
-import java.util.Date;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import nc.noumea.mairie.abs.dto.CompteurAsaDto;
 import nc.noumea.mairie.abs.dto.CompteurDto;
 import nc.noumea.mairie.abs.dto.ReturnMessageDto;
 import nc.noumea.mairie.abs.service.IAgentMatriculeConverterService;
 import nc.noumea.mairie.abs.service.ICounterService;
-import nc.noumea.mairie.abs.transformer.MSDateTransformer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,9 +21,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import flexjson.JSONDeserializer;
-import flexjson.JSONSerializer;
 
 @Controller
 @RequestMapping("/asaA55")
@@ -40,44 +35,45 @@ public class AsaA55Controller {
 	@Autowired
 	private IAgentMatriculeConverterService converterService;
 
+	/**
+	 * Modifie manuellement le compteur ASA A55 d un agent RequestBody : Format
+	 * du type timestamp : "/Date(1396306800000+1100)/"
+	 */
 	@ResponseBody
 	@RequestMapping(value = "/addManual", produces = "application/json;charset=utf-8", method = RequestMethod.POST)
 	@Transactional(value = "absTransactionManager")
-	public ResponseEntity<String> addAsaA55ManuelForAgent(@RequestParam("idAgent") int idAgent,
-			@RequestBody(required = true) String compteurDto) {
+	public ReturnMessageDto addAsaA55ManuelForAgent(@RequestParam("idAgent") int idAgent,
+			@RequestBody(required = true) CompteurDto compteurDto, HttpServletResponse response) {
 
 		logger.debug("entered POST [asaA55/addManual] => addAsaA55ManuelForAgent with parameters idAgent = {}", idAgent);
 
-		CompteurDto dto = new JSONDeserializer<CompteurDto>().use(Date.class, new MSDateTransformer()).deserializeInto(
-				compteurDto, new CompteurDto());
-
 		int convertedIdAgent = converterService.tryConvertFromADIdAgentToSIRHIdAgent(idAgent);
 
-		ReturnMessageDto srm = counterService.majManuelleCompteurToAgent(convertedIdAgent, dto);
-
-		String response = new JSONSerializer().exclude("*.class").deepSerialize(srm);
+		ReturnMessageDto srm = counterService.majManuelleCompteurToAgent(convertedIdAgent, compteurDto);
 
 		if (!srm.getErrors().isEmpty()) {
-			return new ResponseEntity<>(response, HttpStatus.CONFLICT);
-		} else {
-			return new ResponseEntity<>(response, HttpStatus.OK);
+			response.setStatus(HttpServletResponse.SC_CONFLICT);
 		}
+
+		return srm;
 	}
 
+	/**
+	 * Liste des compteurs ASA A55 ResponseBody : Format du type timestamp :
+	 * "/Date(1396306800000+1100)/"
+	 */
 	@ResponseBody
 	@RequestMapping(value = "/listeCompteurA55", produces = "application/json;charset=utf-8", method = RequestMethod.GET)
 	@Transactional(readOnly = true)
-	public ResponseEntity<String> getListeCompteur() {
+	public List<CompteurAsaDto> getListeCompteur() {
 
 		logger.debug("entered GET [asaA55/listeCompteurA55] => getListeCompteur ");
 
 		List<CompteurAsaDto> result = counterService.getListeCompteur();
 
 		if (result.size() == 0)
-			return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
+			throw new NoContentException();
 
-		String response = new JSONSerializer().exclude("*.class").transform(new MSDateTransformer(), Date.class)
-				.deepSerialize(result);
-		return new ResponseEntity<String>(response, HttpStatus.OK);
+		return result;
 	}
 }
