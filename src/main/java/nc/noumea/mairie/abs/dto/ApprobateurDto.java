fa3e13bd67c14be30ae4fb7e39442223c0be0b5b
nicno85 <nicno85@ville-noumea.nc>
2015-03-13 00:08:46 +0000

    #14279 : SIRH : Ecran des droits du kiosque beaucoup trop long

---
+++ src/main/java/nc/noumea/mairie/abs/dto/ApprobateurDto.java
@@ -0,0 +1,23 @@
+package nc.noumea.mairie.abs.dto;
+
+public class ApprobateurDto {
+
+	private AgentWithServiceDto approbateur;
+	private AgentDto delegataire;
+
+	public AgentDto getDelegataire() {
+		return delegataire;
+	}
+
+	public void setDelegataire(AgentDto delegataire) {
+		this.delegataire = delegataire;
+	}
+
+	public AgentWithServiceDto getApprobateur() {
+		return approbateur;
+	}
+
+	public void setApprobateur(AgentWithServiceDto approbateur) {
+		this.approbateur = approbateur;
+	}
+}
