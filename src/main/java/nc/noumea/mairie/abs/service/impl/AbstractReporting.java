31f8c10884a685d6aadd9eb3df77c6740a8a27e7
nicno85 <nicno85@ville-noumea.nc>
2016-10-10 00:05:52 +0000

    #33713 : MAJ dependances

--- src/main/java/nc/noumea/mairie/abs/service/impl/AbstractReporting.java
+++ src/main/java/nc/noumea/mairie/abs/service/impl/AbstractReporting.java
@@ -2,17 +2,18 @@ package nc.noumea.mairie.abs.service.impl;
 
 import java.util.List;
 
-import com.lowagie.text.Chunk;
-import com.lowagie.text.Document;
-import com.lowagie.text.DocumentException;
-import com.lowagie.text.Element;
-import com.lowagie.text.Font;
-import com.lowagie.text.FontFactory;
-import com.lowagie.text.Paragraph;
-import com.lowagie.text.Rectangle;
-import com.lowagie.text.pdf.PdfPCell;
-import com.lowagie.text.pdf.PdfPTable;
-import com.lowagie.text.pdf.PdfPageEventHelper;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfPageEventHelper;
+
 
 public abstract class AbstractReporting extends PdfPageEventHelper {
 
--- src/main/java/nc/noumea/mairie/abs/service/impl/TitreDemandeReportingService.java
+++ src/main/java/nc/noumea/mairie/abs/service/impl/TitreDemandeReportingService.java
@@ -5,25 +5,26 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-import nc.noumea.mairie.abs.dto.AgentWithServiceDto;
-import nc.noumea.mairie.abs.dto.EditionDemandeDto;
-import nc.noumea.mairie.abs.service.ITitreDemandeReportingService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.lowagie.text.Document;
-import com.lowagie.text.DocumentException;
-import com.lowagie.text.Element;
-import com.lowagie.text.Font;
-import com.lowagie.text.Image;
-import com.lowagie.text.PageSize;
-import com.lowagie.text.Paragraph;
-import com.lowagie.text.Rectangle;
-import com.lowagie.text.pdf.PdfPCell;
-import com.lowagie.text.pdf.PdfPTable;
-import com.lowagie.text.pdf.PdfWriter;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import nc.noumea.mairie.abs.dto.AgentWithServiceDto;
+import nc.noumea.mairie.abs.dto.EditionDemandeDto;
+import nc.noumea.mairie.abs.service.ITitreDemandeReportingService;
+
 
 @Service
 public class TitreDemandeReportingService extends AbstractReporting implements ITitreDemandeReportingService {
